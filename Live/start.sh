18a39fbdc1fac0ca0ac593610157ece019d960f1
James Michael Parry <jamesmparry87@gmail.com>
2025-07-28 07:44:27 +0100

    Add discord to the run step

--- Live/start.sh
+++ Live/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd Live
 which python3 && python3 --version
+python3 -m pip install --upgrade discord.py
 python3 -m pip show discord.py
 python3 -m pip list
 python3 ash_bot_fallback.py
